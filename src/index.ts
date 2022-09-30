0a9ce9993457423b2ba4918619b93339a630caf3
Paul Donchenko <paul@pjdon.dev>
2022-09-30 16:37:06 -0400

    working star import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import * as acorn from 'acorn';
+
 const greetingTarget: string = "World";
 
 console.log(`Hello ${greetingTarget}!`);
+
+const code = `\
+const msg = "Hello World!";
+console.log(msg);
+`;
+const result = acorn.parse(code, {ecmaVersion: "latest"});
+
+console.log("Done");
